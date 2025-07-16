f17b99e9f1ae0e8c086c4b3559d65eb507b9575b
hareesh Sureddy <hareeshsureddy@gmail.com>
2025-07-16 23:07:09 +0530

    16/07/2025

--- 0013.AMD/main.c
+++ 0013.AMD/main.c
@@ -2,6 +2,12 @@
 #include <stdlib.h>
 
 #define GPIOA 0x80000000
+/* F2F
+C programming:-
+1.Shift even and odd bits in a unsigned 32bit value.
+2.Write a program to know the frequency of the elements present in an array. Input array[]={1,2,2,3,1}. write in a optimized way.  Followup:- Write dynimacally, don’t use extra space.
+3.Write a function to check the 5th bit of an 32 bit data, and make sure that 10th is same as a 5th bit. And same way check the 15th bit of an 32 bit data, and make sure that 20th is same as a 15th bit
+*/
 int main()
 {
     //Toggel a LED wich is connected to GPIO 1 and 5
---
+++ 0015.AMD Hyderabed/main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+C programming:-
+1.Shift even and odd bits in a unsigned 32bit value.
+2.Write a program to know the frequency of the elements present in an array. Input array[]={1,2,2,3,1}. write in a optimized way.  Followup:- Write dynimacally, don’t use extra space.
+3.Write a function to check the 5th bit of an 32 bit data, and make sure that 10th is same as a 5th bit. And same way check the 15th bit of an 32 bit data, and make sure that 20th is same as a 15th bit
+*/
+int main()
+{
+    printf("Hello world!\n");
+    return 0;
+}
